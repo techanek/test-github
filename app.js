99a1feda940e34d701ab2576052ed967b6079fa9
Pradip Khuman <pradip.khuman@techanek.com>
2025-09-26 17:49:11 +0530

    first commit

--- app.js
+++ app.js
@@ -7,22 +7,26 @@ const APP_HOST = '127.0.0.1';
 const APP_PORT = 8080;
 
 app.use((req, res) => {
-    // Get real client IP
-    const ip = req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	// Get real client IP
+	const ip = req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-    // GeoIP lookup
-    const geo = geoip.lookup(ip) || {};
-    const country = geo.country || 'unknown';
-    const city = geo.city || 'unknown';
+	// Log the IP used for Geo lookup
+	console.log(`Geo lookup for IP: ${ip}`);
 
-    // Forward request to main app
-    const url = `http://${APP_HOST}:${APP_PORT}${req.originalUrl}`;
-    const headers = Object.assign({}, req.headers, {
-        'X-Geo-Country': country,
-        'X-Geo-City': city
-    });
+	// GeoIP lookup
+	const geo = geoip.lookup(ip) || {};
+	const country = geo.country || 'unknown';
+	const city = geo.city || 'unknown';
 
-    req.pipe(request({ url, headers })).pipe(res);
+	// Forward request to main app
+	const url = `http://${APP_HOST}:${APP_PORT}${req.originalUrl}`;
+	const headers = Object.assign({}, req.headers, {
+		'X-Geo-Country': country,
+		'X-Geo-City': city,
+		'X-Geo-IP': ip
+	});
+
+	req.pipe(request({ url, headers })).pipe(res);
 });
 
 app.listen(9000, () => console.log('GeoIP sidecar running on 9000'));
--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const app = express();
 
 // catch all routes safely
 app.use((req, res) => {
-    res.send(`Main app received X-Geo-Country=${req.headers['x-geo-country'] || 'none'}, X-Geo-City=${req.headers['x-geo-city'] || 'none'}`);
+	const country = req.headers['x-geo-country'] || 'none';
+	const city = req.headers['x-geo-city'] || 'none';
+	const ip = req.headers['x-geo-ip'] || 'unknown';
+	res.send(`Main app received X-Geo-Country=${country}, X-Geo-City=${city}, X-Geo-IP=${ip}`);
 });
 
 app.listen(8080, () => console.log('Main app running on 8080'));
